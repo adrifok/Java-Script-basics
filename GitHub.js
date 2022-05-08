caded4829ec22e0b3b276f51c5d41fda90030b43
AdrianA <97637423+adrifok@users.noreply.github.com>
2022-05-08 19:30:28 -0300

    Add files via upload

---
+++ GitHub.js
@@ -0,0 +1,34 @@
+/*
+Fuente https://prep.soyhenry.com/primeros_pasos/
+
+Subir repositorio a GitHub:
+
+git init                        ==>inicia un repo local vacio
+git add .                       ==> agrega archivos a git
+git commit -m ""                ==> confirma los archivos/cambios agregados
+git remote add origin           ==> link del repo remoto (lo saco de github) vincula repo remoto con local
+git main M Branch 
+git push                        ==>sube los cambios a github
+---------------------------------------
+Actualizar cambios en archivos
+
+git add + nombre archivo
+git status
+git commit -m " "
+git push
+---------------------------------------
+Bajar repositorio a PC 
+-en Git Hub clonar repo, copiar link
+-abrir GitBash, boton derecho del mouse y GitBash here
+-escribir git clone y pegar el link del repo
+-abrir con cualquier editor de codigo
+-------------------------------------
+Git Token
+GitHub----> Settings---> Developer Settings--->Personal Access Tokens--->Generate New Token
+en editor de codigo para autenticacion: get push https://<GitHubAccessToken>@github.com/<GitHub_Username>/<RepositoryName>.git   
+(reemplazando en el link nro de token, nombre de usuario y nombre de repo)
+
+
+
+
+/* 
---
+++ Listas Enlazadas.js
@@ -0,0 +1,46 @@
+    /* Fuente students.soyhenry.com/classes/5?cohortID=18 */                
+                                        
+                                        // Head representa la dirección de memoria del PRIMER elemento de la Lista---> Head es un Nodo. 
+                                        //Los Nodos son el camino a recorrer.
+    function Node(data){                // La Lista contiene Next y Data:
+                                        // Next describe la posicion de memoria del prox elemento de la lista
+                                        // Data es la informacion que alojamos en dicha posicion. Data es solo una partecita del Nodo.
+    this.data = data;                   //data y next son nombres por convencion(le puedo llamar con cualquier otro nombre, 
+    this.next = null;                   //ej 'el proximo nodo', 'el datito')
+    }
+
+function LinkedList() {
+    this.length = 0;
+    this.head = null;
+}
+                                            //Como creo una nueva Lista? con un Constructor o con una Clase
+    let list = new newLinkedList;           //Esto es agregar un Nodo 
+
+    function newLinkedList();
+    LinkedList.prototype.add = function(data){ 
+        let node = newNode(data);       //creamos un nuevo Nodo
+
+        let current =this.head;        // this va a ser la instancia de la Linked list a la cual le aplico el metodo, en este caso: LinkedList
+        if(current===null){             //puedo o no tener informacion en el head, si esta vacio(null) no entro 
+                                        // aca estoy en un caso en el que no tengo un nodo asociado al head por lo tanto --> null
+                                        // por lo tanto list --->  null   
+        this.head = node;               //ahora this.head ve el primer nodo y lo asocia ---> this.head = node 
+
+        }                                   
+        while(current.next){             //current es el nodo y mientras que while.current.next sea distinto de null(o sea si ya tengo un dato) 
+        current = current.next;          //voy al proximo Nodo avanzando/recorriendo a lo largo de la lista(Next me permite seguir avanzando)
+        }        
+        current.next = node;             //next apunta al nuevo nodo
+    }
+                    /* Como elimino un Nodo?
+    list .remove ---> quiero eliminar el ultimo Nodo. Para esto apuntamos al penultimo Nodo 
+    (current.next es el ultimo) porque solo puedo ver hacia adelante
+    Si quiero eliminar un Nodo en la 2da posicion (delete(1)) 
+    para eso tengo que reemplazar la posicion  del nodo0 y elimino el current.next y tengo que eliminar la info asociada 
+    creando una variable auxiliar (let aux = current.next) para guardar la info/
+    Me tengo que quedar con una copia de lo que tengo para poder avanzar sin perder informacion*/
+
+
+
+
+
