85f4715f48be06f48c4ef514b50998fd3a40a7fb
AdrianA <97637423+adrifok@users.noreply.github.com>
2022-05-19 20:43:34 -0300

    Update Decimal a Binario.js

--- Decimal a Binario.js
+++ Decimal a Binario.js
@@ -10,7 +10,20 @@ pos 2 1 0       1 x 4     +    1 x 2    +   0 x 1
 
 
 Decimal a Binario 
-          
+          En cuanto al pasaje de decimal a binario, debemos dividir por dos y quedarnos con el resto, dicho 
+resto va a ser quien nos marque el binario involucrado.
+Por ejemplo:
+Realizamos la operación inversa, convertimos 21 en binario.
+
+21/2 = 10 (resto 1)
+10/2 = 5 (resto 0)
+5/2 = 2 (resto 1)
+2/2 = 1 (resto 0)
+1/2 = 0 (resto 1)
+
+Ahora leemos de abajo hacia arriba los restos, al concatenarlos, obtendremos nuestra solución en 
+binario 10101.
+Otro ejemplo:
            E  R
 
 7 = 7 / 2  3  1        3   3   --> 1 es el resto/residuo
